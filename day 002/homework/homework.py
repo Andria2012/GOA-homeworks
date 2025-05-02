a69ff8a68a9be2a928336be573019edc19b21a9d
Andria2012 <andriakhmaladze81@gmail.com>
2025-05-02 16:30:43 +0400

    commit

---
+++ day 002/homework/homework.py
@@ -0,0 +1,52 @@
+"andria" + "khmaladze"
+
+"andria" + "3 khmaladze"
+
+"andria" + "7" + "khmaladze"
+
+"andriaandriaandria" + "khmaladze"
+
+"giorgi" + "khmaladze"
+
+"babi" + "5" +"khmaladze"
+
+"babi" + "8 khmaladze"
+
+"saba" + "4" + "khmaladze"
+
+"sabasabasaba" + "khaladzekhmaladzekhmaladze"
+
+10 - 7
+
+20 - 5
+
+40 - 30
+
+3 - 1
+
+8 - 5
+
+5 / 5
+
+10 / 2
+
+4 / 2
+
+8 / 8
+
+3 / 1
+
+
+print = 0
+
+(count) = +1
+
+(count) = 10
+
+print = (count) + 30
+
+name = input ("giorgi")
+
+print ("hello giorgi")
+
+print ("giorgi")
