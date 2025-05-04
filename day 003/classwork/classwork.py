f9b9ac8ba36c1e0935fdf565383d3f997c3f4391
Andria2012 <andriakhmaladze81@gmail.com>
2025-05-04 11:29:21 +0400

    commit

---
+++ day 003/classwork/classwork.py
@@ -0,0 +1,17 @@
+print("Name: " + "andria")
+print("Age: " + "12")
+print("Height: 1.34")
+print("Hello " + "andria" + "! Your age is " + "12")
+
+
+name = "andria"
+
+
+
+pokemon = "pikachu"
+
+count = 1
+
+count =count + 1
+
+pokemon = "pikachu,charlizard"
---
+++ day 003/homework/homework.py
@@ -0,0 +1,20 @@
+print("hello my name is andria and my surname is khmaladze,i am 12 years old")
+
+print("hello my name is gigi and my surname is gogitidze,i am 16 years old")
+
+print("hello my name is saba and my surname is gogiashvili,i ma 14 years old")
+
+
+
+
+print = "count + 23"
+
+count =  "count + 221"
+
+count = "234"
+
+count=print ("342")
+
+count = count + "323"
+
+
