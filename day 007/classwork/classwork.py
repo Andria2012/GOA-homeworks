a0322603c7b9ac3d318b4e0345cf1a8ad665fed2
Andria2012 <andriakhmaladze81@gmail.com>
2025-05-17 21:10:11 +0400

    commit

---
+++ day 007/classwork/classwork.py
@@ -0,0 +1,24 @@
+while True :
+    print("holaa")
+
+
+
+while 1<100:
+        print("")
+
+
+1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100
+
+
+
+
+# A "while" loop in programming is a control flow statement that repeatedly executes a block of code as long as a given condition is true. It continues running the code within the loop until the condition becomes false, at which point the loop stops and the program continues with the next statement. 
+
+
+
+
+while 1<10:
+      print("hi")
+
+
+1,2,3,4,5,6,7,8,9,10
---
+++ day 007/homework/homework.py
@@ -0,0 +1,48 @@
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+
+
+
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+
+
+
+
+
+
+
+# A "while" loop in programming is a control flow statement that repeatedly executes a block of code as long as a given condition is true. It continues to loop until the condition becomes false, at which point the loop terminates. This is useful when you don't know in advance how many times a specific code section needs to be run. 
+
+
+
+
+password = "python123"
+
+user_password = input("enter your password:  ")
+
+while user_password != password:
+    user_password = input("enter you password again: ")
+
+print("welcome")
+
+
+
+
+
+n = int(input("შეიყვანე რიცხვი n: "))
+i = 1
+sum = 0
+
+while i <= n:
+    sum += i
+    i += 1
+
+print("ჯამი არის:", sum)
