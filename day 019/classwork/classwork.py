c89f12e50f842b030915144e579154c5563394ef
Andria2012 <andriakhmaladze81@gmail.com>
2025-06-26 14:45:37 +0400

    commit

---
+++ day 019/classwork/classwork.py
@@ -0,0 +1,14 @@
+name = ["1,2,3,4,5,6,7,8,9,10,alexandre"]
+num = [("alexandre")]
+num = [-2]
+
+
+
+#tringi ari sadac iwereba winadadeba frchxilshi an brwyalebshi.
+#immutable aris sadac vwert arac ricxvebs.
+#listi aris igive rac []-es
+
+
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+print("arr5.12")
