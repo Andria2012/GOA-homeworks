02cdace4a3dc2f60bdcd258bef2ff97034f1630d
Andria2012 <andriakhmaladze81@gmail.com>
2025-07-02 15:52:51 +0400

    commit

---
+++ day 021/classwork/classwork.py
@@ -0,0 +1,24 @@
+age = int(input("enter your age: "))
+
+while True:
+    age = (1,2,3,4,5)
+else:
+    print("you are not ready")
+
+
+
+
+for i in range(10,1):
+    print(i)
+
+
+
+while i in range(0,6):
+    print("me var nadiri")
+
+
+
+
+
+
+
---
+++ day 021/homework/homework.py
@@ -0,0 +1,24 @@
+n = int(input("enter your number: "))
+
+while True:
+    print(n+1)
+
+
+
+
+num = int(input("enter your number: "))
+
+while True:
+    print("2,3,4,5,6,7")
+
+
+
+
+for i in range(1,100):
+    print(i)
+
+
+
+
+for i in range(0,6):
+    print(i)
