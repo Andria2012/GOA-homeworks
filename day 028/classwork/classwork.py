e744364e7afb5a94061a34950a72f00dd48634b5
Andria2012 <andriakhmaladze81@gmail.com>
2025-07-26 21:18:19 +0400

    commit

---
+++ day 028/classwork/classwork.py
@@ -0,0 +1,5 @@
+#The capitalize() method returns a string where the first character is upper case, and the rest is lower case.
+
+#წერტილოვანი ნოტაცია არის სინტაქსი, რომელიც გამოიყენება პროგრამირების ენებში, მათ შორის Python-ში, ობიექტის ატრიბუტებსა და მეთოდებზე წვდომისთვის.
+
+#ფუნქციები და მეთოდები len(), append(), insert() და pop() ხშირად გამოიყენება Python-ში მონაცემთა სტრუქტურების, განსაკუთრებით კი სიების მანიპულირებისთვის.
---
+++ day 028/homework/homework.py
@@ -0,0 +1,16 @@
+text = "pizza time".upper()
+print(text)
+
+
+
+name = "andria".upper()
+print(name)
+
+
+
+
+my_list = []
+my_list.append(5)
+print(my_list)
+
+
