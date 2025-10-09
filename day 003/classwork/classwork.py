9b5cc69129b2149efcf55d4a1454c546c8eb2749
Andria2012 <andriakhmaladze81@gmail.com>
2025-10-09 17:45:00 +0400

    commit

--- day 003/classwork/classwork.py
+++ day 003/classwork/classwork.py
@@ -1,18 +1,29 @@
-user_name = "andria"
-print(user_name)
+name = input("enter your name:")
+print("hello" + "" + name)
 
-user_name = "gio"
-print(user_name)
+name = input("enter your name: ")
+age = input("enter your age: ")
+print(name + "" + age)
+ 
+ color = input("რა არის შენი საყვარელი ფერი? ")
+number = input("შეიყვანე საყვარელი რიცხვი: ")
+animal = input("რა არის შენი საყვარელი ცხოველი? ")
+color = input("რა არის შენი საყვარელი ფერი? ")
+number = input("შეიყვანე საყვარელი რიცხვი: ")
+animal = input("რა არის შენი საყვარელი ცხოველი? ")
 
-userName = "saba"
-print(userName)
+print(f"\nშენი საყვარელი ფერია {color}, რაც ძალიან ლამაზად נשვერს!")
+print(f"რიცხვი {number} ყოველთვის მოგაქვს იღბალს!")
+print(f"და ბოლოს, შენი საყვარელი ცხოველია {animal}, რომელიც ალბათ ძალიან საყვარელია!")
+print(f"შეიძლება ითქვას, რომ {color} {animal} {number}-ჯერ უფრო განსაკუთრებულია!")
 
-userName = "nika"
-print(userName)
+item1 = input("შეიყვანე პირველი პროდუქტი: ")
+item2 = input("შეიყვანე მეორე პროდუქტი: ")
+item3 = input("შეიყვანე მესამე პროდუქტი: ")
+item4 = input("შეიყვანე მეოთხე პროდუქტი: ")
 
-#snake_case-ს იყენებენ პითონში
-#camel_case-ს იყენებენ ძირითადად ჯავა სკრიპტზე
-
-name = str(input("enter your name: "))
-username = str(input("enter your username: "))
-print("hello" + " " + name + " " + username)
+print("\nList:")
+print(f"1. {item1}")
+print(f"2. {item2}")
+print(f"3. {item3}")
+print(f"4. {item4}")
--- day 003/homework/homework.py
+++ day 003/homework/homework.py
@@ -1,33 +1,10 @@
-age = int(input("შეიყვანე შენი ასაკი: "))
-print("შენი ასაკია:", age)
+print("###########")
+print("##salami###")
+print("###########")
 
+name = input("enter your name: ")
+year = input("enter your year: ")
+print("hello" + " " + name + " " + "you are" + " " + year)
 
-name = "andria"
-
-print("hello" +  "  " + name)
-
-
-#input-ი არის პროგრამის შესრულების დროს მომხმარებლისგან მონაცემების მისაღებად.
-
-#output-ი არის პროგრამა წარმოქმნის და აგზავნის გარე დანიშნულების ადგილამდე.
-
-name = "gurami"
-
-print(name)
-
-age = int(input("Enter your age: "))
-year = int(input("which year are you living? "))
-
-print("in 10 years you will be",age + 10, "years old", "and there will be", year+10,"year")
-
-
-
-#snake_case არის დასახელების კონვენცია, სადაც ფრაზაში სიტყვები გაერთიანებულია ინტერვალების გარეშე
-
-#camel_case არის სახელდების კონვენცია, სადაც მრავალსიტყვიან იდენტიფიკატორში სიტყვები ერთმანეთთან დაკავშირებულია ინტერვალების გარეშე
-
-7name = andria
-9name = "gio
-
-name = "andria"
-name = "gio"
+food = input("enter you favorite food: ")
+print(food + " " +"i love food too")
