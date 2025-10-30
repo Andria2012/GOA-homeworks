8c68aa70a50396a054d859ac5be9bc9dedc47ebe
Andria2012 <andriakhmaladze81@gmail.com>
2025-10-30 19:15:58 +0400

    commit

--- day 009/classwork/classwork.py
+++ day 009/classwork/classwork.py
@@ -1,31 +1,26 @@
-i = 1
-while i <= 10:
-    print(i)
-    i += 1
+hour = int(input("შეიყვანე საათი: "))
 
+if hour < 10:
+    print("დილამშვიდობისა")
+elif hour < 20:
+    print("საღამო მშვიდობისა")
+else:
+    print("ღამე მშვიდობისა")
 
+temperature = int(input("შეიყვანე ტემპერატურა: "))
 
+if temperature < 0:
+    print("ცივა")
+elif temperature < 15:
+    print("გრილა")
+else:
+    print("ცხელა")
 
+score = int(input("შეიყვანე ქულა: "))
 
-i = 10
-while i >= 1:
-    print(i)
-    i -= 1
-
-
-
-
-# ცვლადის შექმნა საწყისი მნიშვნელობით
-i = 1
-
-# while ციკლი იმუშავებს სანამ i პატარაა ან ტოლია 5-ზე
-while i <= 5:
-    print(i)       # გამოიტანს მიმდინარე მნიშვნელობას ეკრანზე
-    i += 1         # ყოველ ჯერზე i-ს მნიშვნელობა გაიზრდება 1-ით
-
-
-
-
-
-while True:
-    print("hi")
+if score > 90:
+    print("ყოჩაღ")
+elif score < 50:
+    print("უკეთესი შეიძლება")
+else:
+    print("ჩაიჭერი")
--- day 009/homework/homework.py
+++ day 009/homework/homework.py
@@ -1,1 +1,51 @@
-#new pet
+num = int(input("შეიყვანეთ რიცხვი: "))
+
+if num % 2 == 0:
+    print("რიცხვი ლუწია.")
+else:
+    print("რიცხვი კენტია.")
+
+num = int(input("შეიყვანეთ რიცხვი: "))
+
+if num < 0:
+    print("რიცხვი უარყოფითია.")
+else:
+    print("რიცხვი ნულის ტოლი ან მეტია.")
+
+num = int(input("შეიყვანეთ რიცხვი: "))
+
+if num % 2 == 0:
+    print("რიცხვი ლუწია.")
+else:
+    print("რიცხვი კენტია.")
+
+if num < 0:
+    print("რიცხვი უარყოფითია.")
+else:
+    print("რიცხვი ნულის ტოლი ან მეტია.")
+
+age = int(input("შეიყვანეთ თქვენი ასაკი: "))
+
+if age < 18:
+    print("დაბლოკილი ხარ.")
+else:
+    print("ზრდასრული ხარ.")
+
+color = input("შეიყვანეთ ფერი (მწვანე, ყვითელი, წითელი): ").lower()
+
+if color == "მწვანე":
+    print("წადი.")
+elif color == "ყვითელი":
+    print("მოემზადე.")
+else:
+    print("გაჩერდი.")
+
+price = 22.50
+money = float(input("შეიყვანეთ რამდენი ფული გაქვთ: "))
+
+if money >= price:
+    change = money - price
+    print("ხურდა:", change, "ლარი")
+else:
+    missing = price - money
+    print("გაკლია:", missing, "ლარი")
