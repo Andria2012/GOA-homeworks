389803ab6c3d276da5794be2d2033db49ebcd7f2
Andria2012 <andriakhmaladze81@gmail.com>
2025-11-18 16:42:11 +0400

    commit

--- day 013/homework/homework.py
+++ day 013/homework/homework.py
@@ -1,1 +1,59 @@
-# mxolod sololearn
+print("დაპრინტეთ რიცხვები 1-დან 15-მდე")
+for i in range(16): 
+    print(i)
+
+
+
+
+print("დაპრინტე რიცხი 1-დან 20-მდე")
+for i in range(21): 
+    print(i)
+
+
+
+
+
+
+print("daprinte 1 dan -10 jer:")
+for i in  range(11):
+    print(i)
+
+
+
+
+    
+
+print("დაპრინტეთ რიცხვები 1-დან 10-მდე:")
+for i in range(1, 11):
+    if i % 2 == 1:
+        print(i)
+
+
+
+
+x = int(input("შეიყვანე რიცხვი x: "))
+i = 1
+while i <= x:
+    print(i)
+    i += 1
+x = int(input("შეიყვანე რიცხვი x: "))
+i = 1
+while i <= x:
+    print(i)
+    i += 1
+i = 2
+while i < 100:
+    print(i)
+    i += 2
+
+i = 1
+count = 0
+while i <= 50:
+    if i % 2 == 0:
+        count += 1
+    i += 1
+print("ლუწი რიცხვების რაოდენობაა:", count)
+i = 0
+while i <= 100:
+    print(i)
+    i += 10
