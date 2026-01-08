acbd77db903e7c05a8e97d28917a2901a0bd4141
Andria2012 <andriakhmaladze81@gmail.com>
2026-01-08 17:56:32 +0400

    commit

--- day 025/classwork/classwork.py
+++ day 025/classwork/classwork.py
@@ -1,1 +1,15 @@
-#no classwork
+if 6>4:
+    print(True)
+else:
+    print(False)
+
+def total (a,b,c):
+    return a+b
+if a > b:
+    print(patara xar)
+else:
+    print(didi xar)
+
+def total (a,b):
+    return a+b
+    print(total(5,10))
