1d2a22badc913603de25909828ada32d6c85d12b
Andria2012 <andriakhmaladze81@gmail.com>
2026-01-13 20:56:16 +0400

    commit

--- day 028/classwork/classwork.py
+++ day 028/classwork/classwork.py
@@ -1,5 +1,40 @@
-#The capitalize() method returns a string where the first character is upper case, and the rest is lower case.
+lst = ["Gamarjoba", 6, 8.9, True]
+lst.append("Python")
+print(lst)
 
-#წერტილოვანი ნოტაცია არის სინტაქსი, რომელიც გამოიყენება პროგრამირების ენებში, მათ შორის Python-ში, ობიექტის ატრიბუტებსა და მეთოდებზე წვდომისთვის.
+text = "sup lil kids"
+print(len(text))
 
-#ფუნქციები და მეთოდები len(), append(), insert() და pop() ხშირად გამოიყენება Python-ში მონაცემთა სტრუქტურების, განსაკუთრებით კი სიების მანიპულირებისთვის.
+lst = [1, 2, 3]
+print(len(lst))
+
+sentence = "I love python"
+words = sentence.split()
+print(words)
+
+lst = ['h', 'j', 'k']
+result = "".join(lst)
+print(result)
+
+text = "html is best"
+print(text.upper())
+
+text = "THE CHAD IS BEST"
+print(text.lower())
+
+word = "goa"
+print(word.capitalize())
+
+lst = [1, 2, 3]
+lst.remove(3)
+print(lst)
+
+var1 = "Gamarjoba"
+var2 = 5
+var3 = 6.7
+var4 = True
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
--- day 028/homework/homework.py
+++ day 028/homework/homework.py
@@ -1,16 +1,50 @@
-text = "pizza time".upper()
-print(text)
+sentence = "python and html is best guys"
+words = sentence.split()
+print(words)
 
+text = "gamarjoba"
+print(text.upper())
 
+text = "KARGAD"
+print(text.lower())
 
-name = "andria".upper()
-print(name)
+lst = ["Gamarjoba", 6, 8.9, True]
+lst.append("Python")
+print(lst)
 
+text = "sup lil kids"
+print(len(text))
 
+lst = [1, 2, 3]
+print(len(lst))
 
+sentence = "I love python"
+words = sentence.split()
+print(words)
 
-my_list = []
-my_list.append(5)
-print(my_list)
+lst = ['h', 'j', 'k']
+result = "".join(lst)
+print(result)
 
+text = "html is best"
+print(text.upper())
 
+text = "THE CHAD IS BEST"
+print(text.lower())
+
+word = "goa"
+print(word.capitalize())
+
+lst = [1, 2, 3]
+lst.remove(3)
+print(lst)
+
+var1 = "Gamarjoba"
+var2 = 5
+var3 = 6.7
+var4 = True
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
